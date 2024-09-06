e06dbad1b0815f981326cbf44c3ee2dee9b3a8ec
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-09-06 17:31:18 -0300

    accept multi arity function

--- src/joy/unfix/postfix.clj
+++ src/joy/unfix/postfix.clj
@@ -1,8 +1,15 @@
 (ns joy.unfix.postfix)
 
+(defn arity [f]
+  (alength (.getParameterTypes (first (.getDeclaredMethods (class f))))))
+
 (defn postfix [& e]
   (reduce #(if (fn? %2)
-             (let [[l r & m]%]
-               (cons (%2 r l) m))
-             (cons %2 %))[]e))
+             (let [n (arity %2)
+                   args (take n %)
+                   r (drop n %)]
+               (cons (apply %2 (reverse args)) r))
+             (cons %2 %))
+          []
+          e))
 
--- test/joy/unfix/postfix_test.clj
+++ test/joy/unfix/postfix_test.clj
@@ -1,8 +1,18 @@
 (ns joy.unfix.postfix-test
   (:use [clojure.test :only [deftest is]])
-  (:use [joy.unfix.postfix :only [postfix]]))
+  (:use [joy.unfix.postfix :only [postfix postfix2]]))
 
 (deftest test-postfix
   (is (= [14] (postfix 5 1 2 + 4 * + 3 -)))
   (is (= [9]  (postfix 3 2 1 + *)))
   (is (= [7]  (postfix 1 2 3 * +))))
+
+(defn sum! [a b c] (+ a c b))
+(defn max> [a b] (max a b))
+
+(deftest test-postfix-2
+  (is (= [8] (postfix 5 1 2 sum!)))
+  (is (= [1 8] (postfix 5 1 2 sum! 1)))
+  (is (= [2 5] (postfix 5 1 2 max>))))
+
+
